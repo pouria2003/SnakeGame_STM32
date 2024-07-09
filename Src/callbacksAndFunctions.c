7d67f209540f81a2ac6cf49ea468ba6bd61936a6
pouria2003 <s.pouria.h2003@gmail.com>
2024-07-09 14:03:29 +0430

    added uart

--- Src/callbacksAndFunctions.c
+++ Src/callbacksAndFunctions.c
@@ -26,6 +26,12 @@ extern enum Threads;
 
 extern uint8_t tsignals[NTHREADS];
 
+extern UART_HandleTypeDef huart1;
+
+extern char received_byte;
+
+
+
 Snake snake = {NULL};
 
 uint8_t direction = RIGHT;
@@ -142,7 +148,12 @@ void moveSnake() {
 }
 
 
-
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+{
+	if(huart->Instance == USART1){
+		HAL_UART_Receive_IT(&huart1, &received_byte, 1);
+	}
+}
 
 
 
@@ -158,66 +169,71 @@ volatile uint32_t last_gpio_exti;
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
+	HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_8);
   if (last_gpio_exti + 200 > HAL_GetTick()) // Simple button debouncing
   {
     return;
   }
-  last_gpio_exti = HAL_GetTick();
-
-  int8_t row_number = -1;
-  int8_t column_number = -1;
-
-  if (GPIO_Pin == GPIO_PIN_0)
-  {
-    // blue_button_pressed = 1;
-    // return;
-  }
-
-  for (uint8_t row = 0; row < 4; row++) // Loop through Rows
-  {
-    if (GPIO_Pin == Row_pins[row])
-    {
-      row_number = row;
-    }
-  }
-
-  HAL_GPIO_WritePin(Column_ports[0], Column_pins[0], 0);
-  HAL_GPIO_WritePin(Column_ports[1], Column_pins[1], 0);
-  HAL_GPIO_WritePin(Column_ports[2], Column_pins[2], 0);
-  HAL_GPIO_WritePin(Column_ports[3], Column_pins[3], 0);
-
-  for (uint8_t col = 0; col < 4; col++) // Loop through Columns
-  {
-    HAL_GPIO_WritePin(Column_ports[col], Column_pins[col], 1);
-    if (HAL_GPIO_ReadPin(Row_ports[row_number], Row_pins[row_number]))
-    {
-      column_number = col;
-    }
-    HAL_GPIO_WritePin(Column_ports[col], Column_pins[col], 0);
-  }
-
-  HAL_GPIO_WritePin(Column_ports[0], Column_pins[0], 1);
-  HAL_GPIO_WritePin(Column_ports[1], Column_pins[1], 1);
-  HAL_GPIO_WritePin(Column_ports[2], Column_pins[2], 1);
-  HAL_GPIO_WritePin(Column_ports[3], Column_pins[3], 1);
-
-  if (row_number == -1 || column_number == -1)
-  {
-    return; // Reject invalid scan
-  }
-  //   C0   C1   C2   C3
-  // +----+----+----+----+
-  // | 1  | 2  | 3  | 4  |  R0
-  // +----+----+----+----+
-  // | 5  | 6  | 7  | 8  |  R1
-  // +----+----+----+----+
-  // | 9  | 10 | 11 | 12 |  R2
-  // +----+----+----+----+
-  // | 13 | 14 | 15 | 16 |  R3
-  // +----+----+----+----+
-  const uint8_t button_number = row_number * 4 + column_number + 1;
-
-
-  flowHandler(button_number, 0);
+//  last_gpio_exti = HAL_GetTick();
+//
+//  int8_t row_number = -1;
+//  int8_t column_number = -1;
+//
+//  if (GPIO_Pin == GPIO_PIN_0)
+//  {
+//    // blue_button_pressed = 1;
+//    // return;
+//  }
+//
+//  for (uint8_t row = 0; row < 4; row++) // Loop through Rows
+//  {
+//    if (GPIO_Pin == Row_pins[row])
+//    {
+//      row_number = row;
+//    }
+//  }
+//
+//  HAL_GPIO_WritePin(Column_ports[0], Column_pins[0], 0);
+//  HAL_GPIO_WritePin(Column_ports[1], Column_pins[1], 0);
+//  HAL_GPIO_WritePin(Column_ports[2], Column_pins[2], 0);
+//  HAL_GPIO_WritePin(Column_ports[3], Column_pins[3], 0);
+//
+//  for (uint8_t col = 0; col < 4; col++) // Loop through Columns
+//  {
+//    HAL_GPIO_WritePin(Column_ports[col], Column_pins[col], 1);
+//    if (HAL_GPIO_ReadPin(Row_ports[row_number], Row_pins[row_number]))
+//    {
+//      column_number = col;
+//    }
+////    HAL_GPIO_WritePin(Column_ports[col], Column_pins[col], 0);
+//  }
+//
+////  HAL_GPIO_WritePin(Column_ports[0], Column_pins[0], 1);
+////  HAL_GPIO_WritePin(Column_ports[1], Column_pins[1], 1);
+////  HAL_GPIO_WritePin(Column_ports[2], Column_pins[2], 1);
+////  HAL_GPIO_WritePin(Column_ports[3], Column_pins[3], 1);
+//
+//  if (row_number == -1 || column_number == -1)
+//  {
+//    return; // Reject invalid scan
+//  }
+//  //   C0   C1   C2   C3
+//  // +----+----+----+----+
+//  // | 1  | 2  | 3  | 13  |  R0   9
+//  // +----+----+----+----+
+//  // | 4  | 5  | 6  | 14 |  R1    7
+//  // +----+----+----+----+
+//  // | 7  | 8  | 9  | 15 |  R2    5
+//  // +----+----+----+----+
+//  // | 10 | 11 | 12 | 16 |  R3    3
+//  // +----+----+----+----+
+//
+//  uint8_t button_number = row_number * 3 + column_number + 1;
+//  if(column_number == 3) {
+//	  button_number += 2 * (4-row_number) + 1;
+//  }
+//
+//
+//  flowHandler(button_number, 0);
 
 }
--- Src/capture.py
+++ Src/capture.py
@@ -2,16 +2,16 @@ import serial
 import msvcrt
 
 # Change these values as needed
-port = 'COM1' # ttyUSB in Linux
+port = 'COM15' # ttyUSB in Linux
 baudrate = 9600
 
 # Open the serial port
-# ser = serial.Serial(port, baudrate)
+ser = serial.Serial(port, baudrate)
 
 while 1:
     input_char = msvcrt.getch()
     if input_char == b'\x03': # CTRL + C
         break
         
-    print(input_char)
-    # ser.write(input_char.upper())
+    print(input_char.upper() + b"\n")
+    ser.write(input_char.upper() + b"\n")
